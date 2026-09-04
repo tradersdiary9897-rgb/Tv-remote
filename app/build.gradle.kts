b42ca2c25248e4144bba33480c8eaae7e04b8eb0
tradersdiary9897-rgb <tradersdiary9897@gmail.com>
2026-09-04 20:53:51 +0530

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android { namespace = "com.example.magicgyroremote"; compileSdk = 35
+    defaultConfig { applicationId = "com.example.magicgyroremote"; minSdk = 28; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+}
